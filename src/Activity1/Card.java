415707bcdbf6a37420e273ad8e41042a212cba1e
sheba sow <ssow8171@bths.edu>
2019-02-28 08:36:54 -0500

    ;)

--- src/Activity1/Card.java
+++ src/Activity1/Card.java
@@ -34,7 +34,9 @@ public class Card {
      *                  containing the point value of the card
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        this.rank=cardRank;
+        this.suit=cardSuit;
+        this.pointValue=cardPointValue;
     }
 
 
@@ -43,7 +45,7 @@ public class Card {
      * @return this <code>Card's</code> suit.
      */
     public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.suit;
 
     }
 
@@ -52,7 +54,7 @@ public class Card {
      * @return this <code>Card's</code> rank.
      */
     public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.rank;
 
     }
 
@@ -61,7 +63,7 @@ public class Card {
      * @return this <code>Card's</code> point value.
      */
     public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return pointValue;
 
     }
 
@@ -72,8 +74,9 @@ public class Card {
      *         false otherwise.
      */
     public boolean matches(Card otherCard) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        if (this.suit.equals(otherCard.suit)&&this.rank.equals(otherCard.rank)&&this.pointValue==otherCard.pointValue){
+        return true;
+        return false;
     }
 
     /**
