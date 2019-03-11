1a04dab951aade72d23095ffd49f287511a32f90
yhuang7593 <43302938+yhuang7593@users.noreply.github.com>
2019-03-11 08:43:13 -0400

    Update Shuffler.java

--- src/Activity3/Shuffler.java
+++ src/Activity3/Shuffler.java
@@ -52,7 +52,9 @@ public class Shuffler {
      * @param values is an array of integers simulating cards to be shuffled.
      */
     public static void perfectShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+	k=0;
+	for(j=0,j<cards.length/2,j++){
+		cards[j]=temp;
     }
 
     /**
